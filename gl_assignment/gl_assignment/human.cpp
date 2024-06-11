daab841bc4ffc80e84ed2a171a865b32bda03cdf
leesiha <mlice1030@soongsil.ac.kr>
2024-06-11 19:03:02 +0900

    upgraded anpanman design

--- gl_assignment/gl_assignment/human.cpp
+++ gl_assignment/gl_assignment/human.cpp
@@ -13,9 +13,14 @@
 int viewport;
 GLfloat gYAngle, gXAngle, gZAngle;
 GLint moveX, moveY;
-GLuint texture[2];
+GLuint texture[4];
 GLUquadricObj* body;
 GLUquadricObj* head;
+
+void drawHead(GLuint texture, GLUquadricObj* obj, GLUquadricObj* head);
+void drawBody(GLuint texture, GLUquadricObj* obj);
+void drawArms(GLuint texture1, GLuint texture2, GLUquadricObj* obj, GLfloat* d2);
+void drawLegs(GLuint texture1, GLuint texture2, GLUquadricObj* obj, GLfloat* d2);
 
 /*
 #define COLOR_TEXTURE_NUM 16
@@ -91,9 +96,9 @@ void myLight(void) {
 }
 
 void myDisplay(void) {
-    GLfloat d1[] = { 0.0,0.5,0.83, 1.0 };
-    GLfloat d2[] = { 1.0,1.0,1.0, 1.0 };
-    GLfloat d3[] = { 0.7,0.0,0.0, 1.0 };
+    GLfloat d1[] = { 0.0, 0.5, 0.83, 1.0 };
+    GLfloat d2[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat d3[] = { 0.7, 0.0, 0.0, 1.0 };
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glViewport(0 + viewport, 0 + viewport, 600 - 2 * viewport, 600 - 2 * viewport);
@@ -110,83 +115,142 @@ void myDisplay(void) {
     gluQuadricNormals(obj, GL_SMOOTH);
     gluQuadricTexture(obj, GL_TRUE);  // Enable texturing for this quadric
 
-    // 머리
     glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-    glRotatef(270.0, 1.0, 0.0, 0.0);
-    gluSphere(head, 0.3, 50, 30);
-    glRotatef(90.0, 1.0, 0.0, 0.0);
+    // 머리
+    drawHead(texture[0], obj, head);
 
-    glBindTexture(GL_TEXTURE_2D, texture[1]);
-    glTranslatef(0.0, -0.43, 0.0);
-    glRotatef(270.0, 1.0, 0.0, 0.0);
-    gluSphere(head, 0.209, 30, 20);
-    glRotatef(90.0, 1.0, 0.0, 0.0);
+    // 몸통
+    drawBody(texture[1], obj);
 
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, d2);
-    glTranslatef(0.0, 0.15, 0.0);
-    glRotatef(90, 1.0, 0.0, 0.0);
-    glutSolidTorus(0.025, 0.15, 30, 30);
+    // 팔
+    drawArms(texture[2], texture[3], obj, d2);
 
-    glRotatef(-90, 1.0, 0.0, 0.0);
-    glTranslatef(0.0, -0.17, 0.0);
-
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, d2);
-    glPushMatrix();
-    glTranslatef(-0.09, -0.077, 0.0);
-    glRotatef(75, 0.1, 0.0, 0.0);
-    glRotatef(355.0, 0.0, 1.0, 0.0);
-    gluCylinder(obj, 0.1, 0.07, 0.2, 30, 10);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-0.1, -0.28, 0.06);
-    glRotatef(270, 0.1, 0.0, 0.0);
-    glRotatef(5.0, 0.0, 1.0, 0.0);
-    glutSolidTorus(0.03, 0.06, 30, 30);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.09, -0.075, 0.0);
-    glRotatef(75, 0.1, 0.0, 0.0);
-    glRotatef(3, 0.0, 0.1, 0.0);
-    gluCylinder(obj, 0.1, 0.07, 0.2, 30, 30);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.1, -0.28, 0.06);
-    glRotatef(90, 0.1, 0.0, 0.0);
-    glutSolidTorus(0.03, 0.06, 30, 10);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.13, 0.089, 0.0);
-    glRotatef(90, 0.1, 0.2, 0.0);
-    gluCylinder(obj, 0.07, 0.05, 0.2, 3, 3);
-
-    glTranslatef(0.0, -0.007, 0.2);
-    glutSolidSphere(0.05, 20, 20);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-0.13, 0.089, 0);
-    glRotatef(90, 0.1, -0.2, 0.0);
-    gluCylinder(obj, 0.07, 0.05, 0.2, 3, 3);
-
-    glTranslatef(0.0, -0.008, 0.2);
-    glutSolidSphere(0.05, 20, 20);
-    glPopMatrix();
-
-    glPushMatrix();
-    glRotatef(180, 0.0, 1.0, 0.0);
-    glTranslatef(0.0, -0.008, 0.2);
-    glutSolidSphere(0.05, 20, 20);
-    glPopMatrix();
+    // 다리
+    drawLegs(texture[2], texture[3], obj, d2);
 
     glutSwapBuffers();
     gluDeleteQuadric(obj);
     checkGLError("myDisplay");
 }
+
+void drawHead(GLuint texture, GLUquadricObj* obj, GLUquadricObj* head) {
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glRotatef(270.0, 1.0, 0.0, 0.0);
+    gluSphere(head, 0.29, 50, 30);
+    glRotatef(90.0, 1.0, 0.0, 0.0);
+}
+
+void drawBody(GLuint texture, GLUquadricObj* obj) {
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTranslatef(0.0, -0.60, 0.0);
+    glRotatef(270.0, 1.0, 0.0, 0.0);
+
+// 텍스처 좌표 조정을 위한 코드를 추가합니다.
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+    glScalef(1.0, 0.65, 1.0); // 텍스처의 Y축을 조정하여 압축을 줄입니다.
+    glMatrixMode(GL_MODELVIEW);
+
+    // 구체 대신 원통으로 바꾸고 높이를 줄입니다. (높이를 0.4에서 0.2로 줄임)
+    gluCylinder(obj, 0.174, 0.167, 0.34, 30, 30); // 원통의 바닥 반지름, 윗부분 반지름은 유지하고 높이만 줄임.
+
+    // 텍스처 좌표 원래대로 복원
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glRotatef(90.0, 1.0, 0.0, 0.0);
+
+    // 목
+    GLfloat d2[] = { 1.0, 1.0, 1.0, 1.0 };
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, d2);
+    glTranslatef(0.0, 0.37, 0.0);
+    glRotatef(90, 1.0, 0.0, 0.0);
+    glutSolidTorus(0.025, 0.161, 30, 30);
+
+    glRotatef(-90, 1.0, 0.0, 0.0);
+    glTranslatef(0.0, -0.17, 0.0);
+}
+
+void drawArms(GLuint texture1, GLuint texture2, GLUquadricObj* obj, GLfloat* d2) {
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, d2);
+
+    // 왼팔
+    glPushMatrix();
+    glTranslatef(0.13, 0.095, 0.0);
+    glRotatef(90, 0.1, 0.18, 0.0);
+    gluCylinder(obj, 0.065, 0.05, 0.18, 3, 3);
+    glTranslatef(0.0, 0.007, 0.2);
+    glutSolidSphere(0.04, 20, 20); //팔꿈치
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.33, 0.0, 0.0);
+    glRotatef(-90, -75, 90, 0.0);
+    gluCylinder(obj, 0.05, 0.05, 0.2, 3, 3);
+    glTranslatef(0.0, 0.007, 0.2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glutSolidSphere(0.06, 20, 20); //왼손
+    glPopMatrix();
+
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // 오른팔
+    glPushMatrix();
+    glTranslatef(-0.13, 0.095, 0);
+    glRotatef(90, 0.1, -0.18, 0.0);
+    gluCylinder(obj, 0.065, 0.05, 0.18, 3, 3);
+    glTranslatef(0.0, -0.008, 0.2);
+    glutSolidSphere(0.04, 20, 20); //팔꿈치
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.33, 0.0, 0.0);
+    glRotatef(90, 75, 90, 0.0);
+    gluCylinder(obj, 0.05, 0.05, 0.2, 3, 3);
+    glTranslatef(0.0, 0.007, 0.2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glutSolidSphere(0.06, 20, 20); //오른손
+    glPopMatrix();
+}
+
+void drawLegs(GLuint texture1, GLuint texture2, GLUquadricObj* obj, GLfloat* d2) {
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, d2);
+
+    // 오른다리
+    glPushMatrix();
+    glTranslatef(0.09, -0.2, 0.0);
+    glRotatef(90, 0.1, 0.0, 0.0);
+    glRotatef(8, 0.0, 0.1, 0.0);
+    gluCylinder(obj, 0.05, 0.07, 0.25, 30, 30);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.15, -0.45, 0.0);
+    glRotatef(90, 0.1, 0.0, 0.02);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glutSolidTorus(0.055, 0.04, 30, 10);
+    glPopMatrix();
+
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // 왼다리
+    glPushMatrix();
+    glTranslatef(-0.09, -0.2, 0.0);
+    glRotatef(90, 0.1, 0.0, 0.0);
+    glRotatef(350.0, 0.0, 1.0, 0.0);
+    gluCylinder(obj, 0.05, 0.07, 0.25, 30, 10);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.15, -0.45, 0.0);
+    glRotatef(270, 0.1, 0.0, 0.0);
+    glRotatef(5.0, 0.0, 1.0, -0.03);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glutSolidTorus(0.055, 0.04, 30, 30);
+    glPopMatrix();
+}
+
+
 
 void myKeyboard(unsigned char key, int x, int y)
 {
@@ -281,9 +345,9 @@ private:
         body = gluNewQuadric();
         gluQuadricDrawStyle(body, GLU_FILL);
         gluQuadricTexture(body, GL_TRUE);
-        AUX_RGBImageRec* TextureImage[2] = { NULL, NULL };
+        AUX_RGBImageRec* TextureImage[4] = { NULL, NULL, NULL, NULL };
 
-        // Load first texture
+        // Load head texture
         if ((TextureImage[0] = loadBMP("head.bmp"))) {
             Status = TRUE;
             glGenTextures(1, &texture[0]);
@@ -298,7 +362,7 @@ private:
             printf("loadBMP fail for head.bmp\n");
         }
 
-        // TODO: body 이미지
+        // Load body texture
         if ((TextureImage[1] = loadBMP("body.bmp"))) {
             Status = TRUE;
             glGenTextures(1, &texture[1]);
@@ -313,8 +377,38 @@ private:
             printf("loadBMP fail for body.bmp\n");
         }
 
+        // Load red texture
+        if ((TextureImage[2] = loadBMP("red.bmp"))) {
+            Status = TRUE;
+            glGenTextures(1, &texture[2]);
+            glBindTexture(GL_TEXTURE_2D, texture[2]);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TextureImage[2]->sizeX, TextureImage[2]->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, TextureImage[2]->data);
+            checkGLError("loadGLTextures - red.bmp");
+        }
+        else {
+            printf("loadBMP fail for red.bmp\n");
+        }
+
+        // Load yellow texture
+        if ((TextureImage[3] = loadBMP("yellow.bmp"))) {
+            Status = TRUE;
+            glGenTextures(1, &texture[3]);
+            glBindTexture(GL_TEXTURE_2D, texture[3]);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TextureImage[3]->sizeX, TextureImage[3]->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, TextureImage[3]->data);
+            checkGLError("loadGLTextures - yellow.bmp");
+        }
+        else {
+            printf("loadBMP fail for yellow.bmp\n");
+        }
+
         // Free texture data
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 4; i++) {
             if (TextureImage[i]) {
                 if (TextureImage[i]->data) {
                     free(TextureImage[i]->data);
